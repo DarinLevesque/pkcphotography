3fb47bef8f8caaf3d4045924af10470a67467d50
Darin Levesque <darin@darinlevesque.com>
2016-07-17 20:42:54 -0400

    started to build out contact ofrm via sendgrid

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var sg = require('sendgrid').SendGrid(process.env.SENDGRID_API_KEY)
 // var stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 // uncomment for coming soon page to be present
@@ -27,6 +28,28 @@ router.get('/contact', function(req, res) {
     res.render('contact');
 });
 
+router.post('/contact', function(req, res, next) {
+var name = req.body.name;
+var email = req.body.email;
+var phone = req.body.phone;
+var comments = req.body.comments;
+
+res.send(user_id + ' ' + token + ' ' + geo);
+
+sendgrid.send({
+    to: 'info@pkcphotography.com',
+    from: email,
+    subject: 'Message from PKC contact form',
+    text: 'My first email through SendGrid.' + name + comments
+}, function(err, json) {
+    if (err) {
+        return console.error(err); }
+    console.log(json);
+});
+
+});
+}
+
 router.get('/feature-one', function(req, res) {
     res.render('feature-one');
 });
@@ -35,25 +58,25 @@ router.get('/payment', function(req, res) {
     res.render('payment');
 });
 
-router.post('/charge', function(req, res,next) {
-  var stripeToken = req.body.stripeToken;
-  var amount = req.body.price * 100;
+router.post('/charge', function(req, res, next) {
+    var stripeToken = req.body.stripeToken;
+    var amount = req.body.price * 100;
 
-  // ensure amount === actual product amount to avoid fraud
+    // ensure amount === actual product amount to avoid fraud
 
-  stripe.charges.create({
-    card: stripeToken,
-    currency: 'usd',
-    amount: amount
-  },
-  function(err, charge) {
-    if (err) {
-      console.log(err);
-      res.send('error');
-    } else {
-      res.send('success');
-    }
-  });
+    stripe.charges.create({
+            card: stripeToken,
+            currency: 'usd',
+            amount: amount
+        },
+        function(err, charge) {
+            if (err) {
+                console.log(err);
+                res.send('error');
+            } else {
+                res.send('success');
+            }
+        });
 });
 
 // router.post('/payment', function(req, res) {
